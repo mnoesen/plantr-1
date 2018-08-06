d79463fa3efec50f36cd465b6c9fb0143d00b20b
mnoesen <mnoesen@users.noreply.github.com>
2018-08-06 17:02:08 -0500

    Able to seed veggies and farmers

--- seed.js
+++ seed.js
@@ -1,18 +1,23 @@
-const { db, Vegetable } = require('./models.js');
+const { db, Vegetable, Gardener } = require('./models.js');
 db.sync({ force: true })
   .then(() => {
     return Vegetable.bulkCreate([
-      { name: 'Bell Pepper', color: 'Orange', planted_on: Date.now() },
+      { name: 'Bell_Pepper', color: 'Orange', planted_on: Date.now() },
       { name: 'Turnip', color: 'White', planted_on: Date.now() },
       { name: 'Potato', color: 'Red', planted_on: Date.now() },
       { name: 'Onion', color: 'Yellow', planted_on: Date.now() },
+    ])
+  })
+  .then(() => {
+    return Gardener.bulkCreate([
+      { name: 'Matthew', age: 35, favorite_vegetable: 3},
+      { name: 'Kevin', age: 23, favorite_vegetable: 1}
     ]).then(() => {
       console.log('Database synced');
       //   return Vegetable.findAll();
       // })
     });
   })
-
   .catch(err => {
     console.log('Something went wrong');
     console.log(err);
