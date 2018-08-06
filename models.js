cc6cac1357d494770988917a50b3a50f68d82497
Kevin Gislason <kevingislason@gmail.com>
2018-08-06 16:51:49 -0500

    seeded veggies

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
+const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/plantr');
-module.exports = db;
 
 const Gardener = db.define('gardeners', {
   name: Sequelize.STRING,
@@ -8,18 +8,19 @@ const Gardener = db.define('gardeners', {
 
 const Plot = db.define('plots', {
   size: Sequelize.INTEGER,
-  shaded: Sequelize.BOOLEAN
+  shaded: Sequelize.BOOLEAN,
 });
 
 const Vegetable = db.define('vegetables', {
   name: Sequelize.STRING,
   color: Sequelize.STRING,
-  planted_on: Sequelize.DATE
+  planted_on: Sequelize.DATE,
 });
-
 
 Plot.belongsTo(Gardener);
 Gardener.hasOne(Plot);
-Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'});
-Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'});
-Gardener.belongsTo(Vegetable,  {as: 'favorite_vetetable'});
+Vegetable.belongsToMany(Plot, { through: 'vegetable_plot' });
+Plot.belongsToMany(Vegetable, { through: 'vegetable_plot' });
+Gardener.belongsTo(Vegetable, { as: 'favorite_vetetable' });
+
+module.exports = { db, Plot, Vegetable, Gardener };
--- seed.js
+++ seed.js
@@ -1,8 +1,18 @@
-const db = require('./models.js');
+const { db, Vegetable } = require('./models.js');
 db.sync({ force: true })
   .then(() => {
-    console.log('Database synced');
+    return Vegetable.bulkCreate([
+      { name: 'Bell Pepper', color: 'Orange', planted_on: Date.now() },
+      { name: 'Turnip', color: 'White', planted_on: Date.now() },
+      { name: 'Potato', color: 'Red', planted_on: Date.now() },
+      { name: 'Onion', color: 'Yellow', planted_on: Date.now() },
+    ]).then(() => {
+      console.log('Database synced');
+      //   return Vegetable.findAll();
+      // })
+    });
   })
+
   .catch(err => {
     console.log('Something went wrong');
     console.log(err);
@@ -10,3 +20,19 @@ db.sync({ force: true })
   .finally(() => {
     db.close();
   });
+
+// Vegetable.create({
+//   name: 'Bell Pepper',
+//   color: 'Orange',
+//   planted_on: Date.now(),
+// });
+// Vegetable.create({
+//   name: 'Turnip',
+//   color: 'White',
+//   planted_on: Date.now(),
+// });
+// Vegetable.create({ name: 'Potato', color: 'Red', planted_on: Date.now() });
+// Vegetable.create({
+//   name: 'Onion',
+//   color: 'Yellow',
+//   planted_on: Date.now(),
