53adec1e2c08774b10bc5f5e769b7c19096e8b43
mnoesen <mnoesen@users.noreply.github.com>
2018-08-06 15:59:14 -0500

    Ready to seed

--- models.js
+++ models.js
@@ -1,7 +1,25 @@
-const db = new Sequelize('postgres: //localhost:5432/plantr');
+const db = new Sequelize('postgres://localhost:5432/plantr');
 module.exports = db;
 
-const Gardner = db.define('gardeners' {
+const Gardener = db.define('gardeners', {
   name: Sequelize.STRING,
   age: Sequelize.INTEGER,
-})
+});
+
+const Plot = db.define('plots', {
+  size: Sequelize.INTEGER,
+  shaded: Sequelize.BOOLEAN
+});
+
+const Vegetable = db.define('vegetables', {
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  planted_on: Sequelize.DATE
+});
+
+
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'});
+Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'});
+Gardener.belongsTo(Vegetable,  {as: 'favorite_vetetable'});
